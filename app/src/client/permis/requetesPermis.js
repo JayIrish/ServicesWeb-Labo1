f0817659c93fa551e488475f5702347749a0d0d1
Jason <61055622+JayIrish@users.noreply.github.com>
2022-10-30 15:20:24 -0400

    Update requetesPermis.js

--- app/src/client/permis/requetesPermis.js
+++ app/src/client/permis/requetesPermis.js
@@ -25,7 +25,7 @@ let listerParType = () => {
         dataType:'json',  // json, xml, text
         async: true, // false pour se  mettre en mode synchrone.
         success: (listePermisType) => {
-            afficherPermisType(listePermisType);
+            afficher(listePermisType);
         },
         fail: (e) => {
             alert(`Gros probléme : ${e.message}`);
@@ -43,7 +43,7 @@ let listerParTerr = () => {
         dataType:'json',  // json, xml, text
         async: true, // false pour se  mettre en mode synchrone.
         success: (listePermisTerr) => {
-                afficherPermisTerr(listePermisTerr);
+                afficher(listePermisTerr);
         },
         fail: (e) => {
             alert(`Gros probléme : ${e.message}`);
@@ -65,7 +65,7 @@ let listerParEcheance = () => {
         dataType:'json',  // json, xml, text
         async: true, // false pour se  mettre en mode synchrone.
         success: (listePermisEcheance) => {
-            afficherPermisEcheance(listePermisEcheance);
+            afficher(listePermisEcheance);
         },
         fail: (e) => {
             alert(`Gros probléme : ${e.message}`);
@@ -81,7 +81,7 @@ let permisClassee = () => {
         dataType:'json',  // json, xml, text
         async: true, // false pour se  mettre en mode synchrone.
         success: (listePermisClassee) => {
-            afficherPermisClassee(listePermisClassee);
+            afficher(listePermisClassee);
         },
         fail: (e) => {
             alert(`Gros probléme : ${e.message}`);
